c6cbfdfc7aa4d3e4df592e72956f440bdd41acd7
devin-rivas <devindaboss01@gmail.com>
2024-10-16 20:30:19 +0000

    Lab01 submission

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to CS112");
     }
+
+    public static void name(String name) {
+        name("Devin");
+    }
+
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+    
 }
